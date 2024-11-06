b394afe5bd351c5a470c6eabe8689cfe814ff0fb
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-06 22:06:25 +0900

    test: 동시 요청으로 Race Condition이 발생하는 테스트 작성

--- api/src/test/kotlin/org/example/api/service/CouponServiceTest.kt
+++ api/src/test/kotlin/org/example/api/service/CouponServiceTest.kt
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @SpringBootTest
 class CouponServiceTest {
@@ -38,5 +39,33 @@ class CouponServiceTest {
         val count = couponRepository.count()
 
         assertThat(count).isEqualTo(1)
+    }
+
+    @DisplayName("1000번의 요청이 있는 경우, 100개의 쿠폰만 발행한다.")
+    @Test
+    fun applyMultipleConcurrentCall() {
+        // given
+        val threadCount = 1000
+        val executorService = Executors.newFixedThreadPool(32)
+        val latch = CountDownLatch(threadCount)
+
+        // when
+        for (i in 0..threadCount) {
+            val userId = i.toLong()
+            executorService.submit({
+                try {
+                    couponService.apply(userId)
+                } finally {
+                    latch.countDown()
+                }
+            })
+        }
+
+        latch.await()
+
+        // then
+        val count = couponRepository.count()
+
+        assertThat(count).isEqualTo(100)
     }
 }
