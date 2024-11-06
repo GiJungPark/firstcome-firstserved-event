45dbf0a5543c904d5058564045a65ff1bca5c075
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-11-06 23:54:49 +0900

    feat: API 모듈 Kafka Producer 설정

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.kafka:spring-kafka")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
@@ -30,6 +32,7 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
